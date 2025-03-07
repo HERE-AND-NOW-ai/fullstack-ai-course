a382187bc0d6a5cd62f78daaf15d2958a17a3a37
Ruthran RAGHAVAN <89365498+ruthranraghavan@users.noreply.github.com>
2025-03-07 19:50:26 +0530

    testing streamlit from git

--- your_script.py
+++ your_script.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Welcome to HERE AND NOW AI's Full Stack AI Web Application Course")
-st.write("This is your first AI-powered web application!")
+st.write("You are going to become a Full Stack AI Developer")
